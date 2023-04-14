7e79328263156275a799b959eb05e7e26c387031
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-04-14 17:07:30 -0300

    Add function to get pokemon info, add styles to pokemon list grid

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 const POKE_API = "https://pokeapi.co/api/v2";
+const $cardsContainer = document.querySelector(".cards-container");
 
 function getPokemonList(offset, limit) {
   return fetch(`${POKE_API}/pokemon/?offset=${offset}&limit=${limit}`).then(
     (response) => response.json()
+  );
+}
+
+function getPokemonInfo(pokemon) {
+  return fetch(`${POKE_API}/pokemon/${pokemon}`).then((response) =>
+    response.json()
   );
 }
 
@@ -11,10 +18,27 @@ function showPokemonList() {
     console.log(response);
 
     for (let i = 0; i < response.results.length; i++) {
-      let pokemonCard = document.createElement("div");
-      pokemonCard.textContent = response.results[i].name; //show name and img pokemon
-      document.querySelector(".cards-container").append(pokemonCard);
-      //response.results[i].url  fetchear esto cuando se de vuelta la card
+      getPokemonInfo(response.results[i].name).then((pokemonInfo) => {
+        // console.log(pokemonInfo);
+
+        const pokemonName = response.results[i].name;
+        let pokemonCard = document.createElement("div");
+        let pokemonImageContainer = document.createElement("figure");
+        let pokemonCardImage = document.createElement("img");
+        let pokemonCardName = document.createElement("figcaption");
+
+        pokemonCardImage.setAttribute(
+          "src",
+          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonInfo.id}.png`
+        );
+        pokemonCardImage.setAttribute("alt", pokemonName);
+        pokemonCardName.textContent = pokemonName;
+
+        pokemonImageContainer.append(pokemonCardImage, pokemonCardName);
+        pokemonCard.append(pokemonImageContainer);
+        $cardsContainer.append(pokemonCard);
+        // mostrar stats cuando se de vuelta la card
+      });
     }
   });
 }
