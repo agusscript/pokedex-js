47f5d8a0c12b356dd6171cef25be568677a1bf47
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-04-16 15:03:48 -0300

    Add function to create pokemon card, add loader

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const POKE_API = "https://pokeapi.co/api/v2";
 const $cardsContainer = document.querySelector(".cards-container");
+const offset = 0;
+const limit = 20;
 
 function getPokemonList(offset, limit) {
   return fetch(`${POKE_API}/pokemon/?offset=${offset}&limit=${limit}`).then(
@@ -14,33 +16,46 @@ function getPokemonInfo(pokemon) {
 }
 
 function showPokemonList() {
-  getPokemonList(0, 20).then((response) => {
-    console.log(response);
+  showLoader();
+  getPokemonList(offset, limit).then((pokemonList) => {
+    console.log(pokemonList);
 
-    for (let i = 0; i < response.results.length; i++) {
-      getPokemonInfo(response.results[i].name).then((pokemonInfo) => {
+    for (let i = 0; i < pokemonList.results.length; i++) {
+      getPokemonInfo(pokemonList.results[i].name).then((pokemonInfo) => {
         // console.log(pokemonInfo);
-
-        const pokemonName = response.results[i].name;
-        let pokemonCard = document.createElement("div");
-        let pokemonImageContainer = document.createElement("figure");
-        let pokemonCardImage = document.createElement("img");
-        let pokemonCardName = document.createElement("figcaption");
-
-        pokemonCardImage.setAttribute(
-          "src",
-          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonInfo.id}.png`
-        );
-        pokemonCardImage.setAttribute("alt", pokemonName);
-        pokemonCardName.textContent = pokemonName;
-
-        pokemonImageContainer.append(pokemonCardImage, pokemonCardName);
-        pokemonCard.append(pokemonImageContainer);
-        $cardsContainer.append(pokemonCard);
+        createPokemonCard(pokemonList, pokemonInfo, i);
         // mostrar stats cuando se de vuelta la card
       });
     }
   });
+}
+
+function createPokemonCard(pokemonList, pokemonInfo, index) {
+  occultLoader();
+  const pokemonName = pokemonList.results[index].name;
+  const pokemonCard = document.createElement("div");
+  const pokemonImageContainer = document.createElement("figure");
+  const pokemonCardImage = document.createElement("img");
+  const pokemonCardName = document.createElement("figcaption");
+
+  pokemonCardImage.setAttribute(
+    "src",
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonInfo.id}.png`
+  );
+  pokemonCardImage.setAttribute("alt", pokemonName);
+  pokemonCardName.textContent = pokemonName;
+
+  pokemonImageContainer.append(pokemonCardImage, pokemonCardName);
+  pokemonCard.appendChild(pokemonImageContainer);
+  $cardsContainer.appendChild(pokemonCard);
+}
+
+function showLoader() {
+  document.querySelector(".loader").classList.remove("occult");
+}
+
+function occultLoader() {
+  document.querySelector(".loader").classList.add("occult");
 }
 
 showPokemonList();
