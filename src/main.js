f3362f7c43fc950b59c92716cdd0a7aaf16331d3
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-04-20 23:09:15 -0300

    Add pokemon type with colors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 const POKE_API = "https://pokeapi.co/api/v2";
 let offset = 0;
-let limit = 20;
+let limit = 18;
+
+const typeColors = {
+  grass: "linear-gradient(to right, #6fce40, #103000)",
+  steel: "linear-gradient(to right, #66c0e0, #002e3f)",
+  water: "linear-gradient(to right, #318bff, #00224e)",
+  dragon: "linear-gradient(to right, #5b6eff, #000846)",
+  electric: "linear-gradient(to right, #ffce2c, #3f3000)",
+  ghost: "linear-gradient(to right, #a353a3, #360036)",
+  fire: "linear-gradient(to right, #ff2929, #3a0000)",
+  normal: "linear-gradient(to right, #c0c0c0, #252525)",
+  ice: "linear-gradient(to right, #3fd8ff, #002630)",
+  fighting: "linear-gradient(to right, #ff8000, #291400)",
+  bug: "linear-gradient(to right, #91a119, #1e2200)",
+  psychic: "linear-gradient(to right, #f14179, #3b0013)",
+  rock: "linear-gradient(to right, #afa981, #241f00)",
+  ground: "linear-gradient(to right, #915121, #2c1300)",
+  poison: "linear-gradient(to right, #9141cb, #22003a)",
+  fairy: "linear-gradient(to right, #f170f1, #470047)",
+  dark: "linear-gradient(to right, #585448, #1a1401)",
+  flying: "linear-gradient(to right, #71aeff, #00224e)"
+};
 
 function getPokemonList(offset, limit) {
   return fetch(`${POKE_API}/pokemon/?offset=${offset}&limit=${limit}`).then(
@@ -68,12 +89,14 @@ function createFrontCard(pokemonCard, pokemonInfo) {
 
 function createBackCard(pokemonCard, pokemonInfo) {
   const backCard = document.createElement("div");
-  const pokemonCardName = document.createElement("p");
+  const pokemonTypeText = document.createElement("p");
+  const pokemonType = pokemonInfo.types[0].type.name;
 
-  pokemonCardName.textContent = pokemonInfo.name;
-  pokemonCardName.setAttribute("class", "pokemon-name");
+  pokemonTypeText.textContent = pokemonType;
+  pokemonTypeText.style.background = typeColors[pokemonType];
   backCard.setAttribute("class", "back-card");
-  backCard.appendChild(pokemonCardName);
+  pokemonTypeText.setAttribute("class", "pokemon-type");
+  backCard.appendChild(pokemonTypeText);
 
   createStatBar("hp", pokemonInfo, backCard);
   createStatBar("attack", pokemonInfo, backCard);
@@ -146,14 +169,14 @@ function removePokemonCards() {
 
 document.querySelector(".next").addEventListener("click", () => {
   removePokemonCards();
-  offset += 20;
+  offset += 18;
   showPokemonList();
 });
 
 document.querySelector(".prev").addEventListener("click", () => {
   if (offset != 0) {
     removePokemonCards();
-    offset -= 20;
+    offset -= 18;
     showPokemonList();
   }
 });
