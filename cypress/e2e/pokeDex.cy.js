27899af9f00d0241845e13b838b2a06aff78266b
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-04-23 13:54:14 -0300

    Add test to verify that the main page items are showing

--- cypress/e2e/pokeDex.cy.js
+++ cypress/e2e/pokeDex.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="Cypress"/>
+
+describe("PokeDex tests", () => {
+  before(() => {
+    cy.visit("index.html");
+  });
+
+  it("Verify that the main page items are showing", () => {
+    const loader = cy.get(".loader");
+    
+    loader.should("be.visible");
+    cy.get("header").find("h1").contains("PokeDex");
+    cy.get("main").should("be.visible");
+    cy.get("footer").should("be.visible");
+    loader.should("have.class", "occult");
+  });
+});
