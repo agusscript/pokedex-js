43ea4e101c79d7541a125f286f415209aba80846
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-04-23 18:56:33 -0300

    Add test to find and check front card elements

--- cypress/e2e/pokeDex.cy.js
+++ cypress/e2e/pokeDex.cy.js
@@ -6,12 +6,27 @@ describe("PokeDex tests", () => {
   });
 
   it("Verify that the main page items are showing", () => {
-    const loader = cy.get(".loader");
-    
-    loader.should("be.visible");
+    cy.get(".loader").should("be.visible");
     cy.get("header").find("h1").contains("PokeDex");
     cy.get("main").should("be.visible");
     cy.get("footer").should("be.visible");
-    loader.should("have.class", "occult");
+  });
+
+  const cardsNum = 16;
+
+  it("Find 16 cards", () => {
+    cy.get(".loader").should("have.class", "occult");
+    cy.get(".cards-container")
+      .find(".pokemon-card")
+      .should("have.length", cardsNum);
+  });
+
+  it("Check front card elements", () => {
+    const frontCard = cy.get(".front-card");
+    frontCard.should("be.visible");
+    cy.get(".pokemon-img").should("be.visible").should("have.length", cardsNum);
+    cy.get(".pokemon-name")
+      .should("be.visible")
+      .should("have.length", cardsNum);
   });
 });
