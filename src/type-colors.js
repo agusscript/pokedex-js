d364c2a284f1cf780fa774570e91d13889b3a3e5
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-05-05 09:56:14 -0300

    Change pokemon images, add type-colors module

---
+++ src/type-colors.js
@@ -0,0 +1,20 @@
+export const typeColors = {
+  grass: "linear-gradient(to right, #6fce40, #103000)",
+  steel: "linear-gradient(to right, #66c0e0, #002e3f)",
+  water: "linear-gradient(to right, #318bff, #00224e)",
+  dragon: "linear-gradient(to right, #5b6eff, #000846)",
+  electric: "linear-gradient(to right, #ffce2c, #3f3000)",
+  ghost: "linear-gradient(to right, #a353a3, #360036)",
+  fire: "linear-gradient(to right, #ff2929, #3a0000)",
+  normal: "linear-gradient(to right, #c0c0c0, #252525)",
+  ice: "linear-gradient(to right, #3fd8ff, #002630)",
+  fighting: "linear-gradient(to right, #ff8000, #291400)",
+  bug: "linear-gradient(to right, #91a119, #1e2200)",
+  psychic: "linear-gradient(to right, #f14179, #3b0013)",
+  rock: "linear-gradient(to right, #afa981, #241f00)",
+  ground: "linear-gradient(to right, #915121, #2c1300)",
+  poison: "linear-gradient(to right, #9141cb, #22003a)",
+  fairy: "linear-gradient(to right, #f170f1, #470047)",
+  dark: "linear-gradient(to right, #585448, #1a1401)",
+  flying: "linear-gradient(to right, #71aeff, #00224e)"
+};
--- src/ui.js
+++ src/ui.js
@@ -1,27 +1,4 @@
-const typeColors = {
-  grass: "linear-gradient(to right, #6fce40, #103000)",
-  steel: "linear-gradient(to right, #66c0e0, #002e3f)",
-  water: "linear-gradient(to right, #318bff, #00224e)",
-  dragon: "linear-gradient(to right, #5b6eff, #000846)",
-  electric: "linear-gradient(to right, #ffce2c, #3f3000)",
-  ghost: "linear-gradient(to right, #a353a3, #360036)",
-  fire: "linear-gradient(to right, #ff2929, #3a0000)",
-  normal: "linear-gradient(to right, #c0c0c0, #252525)",
-  ice: "linear-gradient(to right, #3fd8ff, #002630)",
-  fighting: "linear-gradient(to right, #ff8000, #291400)",
-  bug: "linear-gradient(to right, #91a119, #1e2200)",
-  psychic: "linear-gradient(to right, #f14179, #3b0013)",
-  rock: "linear-gradient(to right, #afa981, #241f00)",
-  ground: "linear-gradient(to right, #915121, #2c1300)",
-  poison: "linear-gradient(to right, #9141cb, #22003a)",
-  fairy: "linear-gradient(to right, #f170f1, #470047)",
-  dark: "linear-gradient(to right, #585448, #1a1401)",
-  flying: "linear-gradient(to right, #71aeff, #00224e)"
-};
-
-export function showPageNumber(number) {
-  document.querySelector(".page-number").textContent = number;
-}
+import { typeColors } from "./type-colors.js";
 
 function createFrontCard(pokemonCard, pokemonInfo) {
   const frontCard = document.createElement("div");
@@ -33,7 +10,7 @@ function createFrontCard(pokemonCard, pokemonInfo) {
   pokemonCardName.textContent = pokemonName;
   pokemonCardImage.setAttribute(
     "src",
-    pokemonInfo.sprites.other["official-artwork"].front_default
+    pokemonInfo.sprites.other.dream_world.front_default
   );
   pokemonCardImage.setAttribute("class", "pokemon-img");
   pokemonCardImage.setAttribute("alt", pokemonName);
@@ -144,3 +121,8 @@ export function removePokemonCards() {
     card.remove();
   });
 }
+
+export function showPageNumber(number) {
+  document.querySelector(".page-number").textContent = number;
+}
+
