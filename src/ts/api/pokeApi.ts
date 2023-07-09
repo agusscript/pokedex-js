f9fce1f89df54f4672b60ee70293aa2a6f2f951f
agusscript <agus.sanchez.240@gmail.com>
2023-07-08 22:03:06 -0300

    Fix name of import types in api module

--- src/ts/api/pokeApi.ts
+++ src/ts/api/pokeApi.ts
@@ -1,13 +1,13 @@
-import { Info, List } from "../mappers/types";
+import { PokemonInfo, ListPokemons } from "../mappers/types";
 
 const POKE_API = "https://pokeapi.co/api/v2";
 
-export function fetchPokemonList(offset: number, limit: number): Promise<List> {
+export function fetchPokemonList(offset: number, limit: number): Promise<ListPokemons> {
   const apiUrl = `${POKE_API}/pokemon/?offset=${offset}&limit=${limit}`;
   return fetch(apiUrl).then((response) => response.json());
 }
 
-export function fetchPokemonInfo(pokemon: string): Promise<Info> {
+export function fetchPokemonInfo(pokemon: string): Promise<PokemonInfo> {
   const apiPokemon = `${POKE_API}/pokemon/${pokemon}`;
   return fetch(apiPokemon).then((response) => response.json());
 }
